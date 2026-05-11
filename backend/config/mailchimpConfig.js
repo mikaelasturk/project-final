e73b27a8800c15c823221d9c5e921cc3c919a7d3
Mikaela Sturk <mikaelasturk@gmail.com>
2026-05-11 17:39:15 +0200

    fix: update ISPREMIUM field to return "Premium" or "Gratis" based on user status

--- backend/config/mailchimpConfig.js
+++ backend/config/mailchimpConfig.js
@@ -18,7 +18,7 @@ const toMailchimpMember = (user) => ({
     CITY: user.city,
     MOTIVE: user.justifyMembership,
     // [ ] WORKSTATUS: user.workStatus,
-    ISPREMIUM: user.isPremium,
+    ISPREMIUM: user.isPremium ? "Premium" : "Gratis",
     PREM_START: user.premiumStartDate,
     PREM_END: user.premiumEndDate,
     CREATEDAT: user.userCreatedAt
